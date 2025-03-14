49d96e4462c8be60b4fdeca3e9759a9c7153b0cc
Nathan A. Mahynski <23516749+mahynski@users.noreply.github.com>
2025-03-14 09:29:01 -0400

    Update start_jupyter.sh

--- .devcontainer/start_jupyter.sh
+++ .devcontainer/start_jupyter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /opt/conda/etc/profile.d/conda.sh; # Start a Jupyter Notebook server with no password so we can easily access it
 conda activate project-env;
+python -m ipykernel install --user --name=project-env; # Can be re-run multiple times
 jupyter notebook --port 1234 --ip='*' --NotebookApp.token='' --NotebookApp.password='';
